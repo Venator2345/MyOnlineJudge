b2414dc729e7633c01247e4f75caf9cabac0fa8e
Vitor Martinez <116887382+Venator2345@users.noreply.github.com>
2025-03-16 17:42:29 -0300

    wip: verificando o operando na análise sintática do Assembly 6502.

--- backend/assembler/SyntaxAnalyser6502.js
+++ backend/assembler/SyntaxAnalyser6502.js
@@ -8,9 +8,54 @@ export default class SyntaxAnalyser6502 {
 
     }
 
+    verifyOperand(operand, expectedOperand) {
+        let correct = true;
+
+        if(operand[0] === '#') {
+            correct = expectedOperand.includes('CONST');
+        }
+        else if(!Number.isInteger(parseInt(token[index]))) {
+            correct = expectedOperand.includes('LABEL');
+        }
+        else {
+            
+        }
+
+        return correct;
+    }
+
     verifyCode(code) {
+        let correct, expectedOperand;
         const lexicalAnalyser6502 = new LexicalAnalyser6502();
+
+        code = code.trim();
+        code = code.replace(/\s+/g,' ');
         this.#availableInstructions = lexicalAnalyser6502.availableInstructions;
+        correct = lexicalAnalyser6502.verifyCode(code);
+
+        if(correct) {
+            lines = code.split('\n');
+            for(let i = 0; i < lines.length && correct; i++) {
+                line = lines[i].split(' ');
+
+                // a primeira palavra deve ser uma instrução da linguagem
+                expectedOperand = this.#availableInstructions.get(line[0]);
+                correct = expectedOperand !== undefined;
+
+                if(expectedOperand == []) {
+                    correct = line.length === 1;
+                }
+                else {
+                    for(let j = 1; j < line.length && correct; j++) {
+                        correct = verifyOperand(line[j], expectedOperand);
+                    }   
+                }
+
+                
+            }
+        }
+
+        return correct;
     }
 
 }
