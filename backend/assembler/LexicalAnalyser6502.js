71a1c6ccdbce7b6458d14c3a1e2e764a7dbee404
Vitor Martinez <116887382+Venator2345@users.noreply.github.com>
2025-03-15 22:34:41 -0300

    wip: analisator léxico para Assembly 6502

--- backend/assembler/LexicalAnalyser6502.js
+++ backend/assembler/LexicalAnalyser6502.js
@@ -1,5 +1,133 @@
 export default class LexicalAnalyser6502 {
-    availableInstructions = new Map();
+    #availableInstructions;
+    
 
+    constructor() {
+        this.#availableInstructions = new Map();
+
+        // CONST - endereçamento imediato
+        // ADDR - endereçamento direto
+        // OFFSET - endereçamento com offset por registrador
+
+        // operações load/store
+        this.#availableInstructions.set('lda',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('sta',['ADDR','OFFSET']);
+        this.#availableInstructions.set('ldx',['CONST','ADDR']);
+        this.#availableInstructions.set('stx',['ADDR','OFFSET']);
+        this.#availableInstructions.set('ldy',['CONST','ADDR']);
+        this.#availableInstructions.set('sty',['ADDR','OFFSET']);
+
+        // transferências entre registradores
+        this.#availableInstructions.set('tax',[]);
+        this.#availableInstructions.set('tay',[]);
+        this.#availableInstructions.set('txa',[]);
+        this.#availableInstructions.set('tya',[]);
+
+        // operações com a pilha
+        this.#availableInstructions.set('tsx',[]);
+        this.#availableInstructions.set('txs',[]);
+        this.#availableInstructions.set('pha',[]);
+        this.#availableInstructions.set('php',[]);
+        this.#availableInstructions.set('pla',[]);
+        this.#availableInstructions.set('plp',[]);
+
+        // operações lógicas
+        this.#availableInstructions.set('bit',['ADDR']);
+        this.#availableInstructions.set('and',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('ora',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('eor',['CONST','ADDR','OFFSET']);
+
+        // operações aritméticas
+        this.#availableInstructions.set('adc',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('sbc',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('cmp',['CONST','ADDR','OFFSET']);
+        this.#availableInstructions.set('cpx',['CONST','ADDR']);
+        this.#availableInstructions.set('cpy',['CONST','ADDR']);
+
+        // incremento e decremento
+        this.#availableInstructions.set('inx',[]);
+        this.#availableInstructions.set('iny',[]);
+        this.#availableInstructions.set('dex',[]);
+        this.#availableInstructions.set('dey',[]);
+        this.#availableInstructions.set('inc',['ADDR']);
+        this.#availableInstructions.set('dec',['ADDR']);
+
+        // deslocamentos (shifts)
+        this.#availableInstructions.set('asl',['ADDR','OFFSET','ACC']);
+        this.#availableInstructions.set('lsr',['ADDR','OFFSET','ACC']);
+        this.#availableInstructions.set('ror',['ADDR','OFFSET','ACC']);
+        this.#availableInstructions.set('rol',['ADDR','OFFSET','ACC']);
+
+        // saltos e chamadas
+        this.#availableInstructions.set('jmp',['LABEL']);
+        this.#availableInstructions.set('jsr',['LABEL']);
+        this.#availableInstructions.set('rts',[]);
+
+        // branches
+        this.#availableInstructions.set('bcc',['LABEL']);
+        this.#availableInstructions.set('bcs',['LABEL']);
+        this.#availableInstructions.set('beq',['LABEL']);
+        this.#availableInstructions.set('bmi',['LABEL']);
+        this.#availableInstructions.set('bne',['LABEL']);
+        this.#availableInstructions.set('bpl',['LABEL']);
+        this.#availableInstructions.set('bvc',['LABEL']);
+        this.#availableInstructions.set('bvs',['LABEL']);
+
+        // ações com registrador de status
+        this.#availableInstructions.set('clc',[]);
+        this.#availableInstructions.set('cld',[]);
+        this.#availableInstructions.set('cli',[]);
+        this.#availableInstructions.set('clv',[]);
+        this.#availableInstructions.set('sec',[]);
+        this.#availableInstructions.set('sed',[]);
+        this.#availableInstructions.set('sei',[]);
+
+        // outras funções
+        this.#availableInstructions.set('brk',[]);
+        this.#availableInstructions.set('nop',[]);
+        this.#availableInstructions.set('rti',[]);
+    }
+
+    verifyToken(token) {
+        let correct;
+        // verificar se é palavra da linguagem
+        correct = (this.#availableInstructions.get(token) !== undefined);
+
+        // verificar se é um operando válido
+        if(!correct) {
+            let index = 0;
+            if(token[0]==='#') {
+                index++;
+            }
+
+            if(token[index]==='$') {
+                // hexadecimal
+                
+            }
+            else if(token[index]==='%') {
+                // binário
+            }
+            else if(Number.isInteger(parseInt(token[index]))) {
+                // decimal 
+
+            }
+            else {
+                
+            }
+        }
+
+    }
+
+    verifyCode(code) {
+        let correct = true;
+        lines = code.split('\n');
+        for(let i = 0; i < lines.length && correct; i++) {
+            line = lines[i].split(' ');
+            for(let j = 0; j < line.length && correct; j++) {
+                correct = this.verifyToken(line[j]);
+            }
+        }
+        return correct;
+    }
     
 }
