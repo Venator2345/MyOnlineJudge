3f2775b20f642f784a84cc50a30405126aaa9e63
Vitor Martinez <116887382+Venator2345@users.noreply.github.com>
2025-03-24 22:15:27 -0300

    ajustando a conexão para o banco de dados

--- backend/db/database.js
+++ backend/db/database.js
@@ -6,10 +6,16 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true // SSL para a plataforma Railway
+    },
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    connectTimeout: 30000
 });
 
 export default pool;
